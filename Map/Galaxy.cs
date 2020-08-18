c86b348504967fbf43d0b473b880e29f6968a2d8
StadnickiK <kstadnicki@interia.pl>
2020-08-18 23:28:35 +0200

    Update map file for world generation using Random class

--- Map/Galaxy.cs
+++ Map/Galaxy.cs
@@ -4,15 +4,17 @@ using System.Collections.Generic;
 
 public class Galaxy : Spatial
 {
-    // Declare member variables here. Examples:
-    // private int a = 2;
-    // private string b = "text";
 
     [Export]
-    int size = 10;
+    int Size = 40;
 
     public List<StarSystem> StarSystems { get; set; }
 
+    PackedScene StarSystemScene = null;
+
+    public Random Rand { get; set; }
+
+    int Seed = 0;
 
     public enum Type
     {
@@ -21,15 +23,36 @@ public class Galaxy : Spatial
         Irregular
     }
 
+    void InitRand(){
+        Seed = Guid.NewGuid().GetHashCode();
+        Rand = new Random(Seed);
+;    }
+    void Generate(){
+
+        int dist = Rand.Next(10, 20);
+        float angle = Rand.Next(0, 70);
+        for(int i = 0;i < Size; i++){
+            RotateY(angle);
+            var pos = Transform.basis.Xform(new Vector3(0, 0, dist));
+            var starSystem = (StarSystem)StarSystemScene.Instance();
+            starSystem.Rand = Rand;
+            var temp = starSystem.Transform;
+            temp.origin = pos;
+            starSystem.Transform = temp;
+            AddChild(starSystem);
+            dist += Rand.Next(300, 500);
+            angle += Rand.Next(0, 50);
+        }
+        Rotation = Vector3.Zero;
+    }
+
+
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
     {
-        
+        StarSystemScene = (PackedScene)GD.Load("res://Map/StarSystem.tscn");
+        InitRand();
+        Generate();
     }
 
-//  // Called every frame. 'delta' is the elapsed time since the previous frame.
-//  public override void _Process(float delta)
-//  {
-//      
-//  }
 }
--- Map/StarSystem.cs
+++ Map/StarSystem.cs
@@ -4,9 +4,6 @@ using System.Collections.Generic;
 
 public class StarSystem : Spatial
 {
-    // Declare member variables here. Examples:
-    // private int a = 2;
-    // private string b = "text";
 
     [Export]
     int Size = 10;
@@ -19,6 +16,8 @@ public class StarSystem : Spatial
 
     List<Planet> Planets = new List<Planet>();
 
+    public Random Rand { get; set; } = new Random();
+
     void LoadScenes(){
         SunScene = (PackedScene)GD.Load("res://Map/Star.tscn");
         PlanetScene = (PackedScene)GD.Load("res://Map/Planet.tscn");
@@ -28,19 +27,18 @@ public class StarSystem : Spatial
         var sun = SunScene.Instance();
         AddChild(sun);
 
-        int dist = 5;
-        float angle = 45f;
+        int dist = Rand.Next(5, 15);
+        float angle = Rand.Next(0, 70);
         for(int i = 0;i < Size; i++){
             RotateY(angle);
             var pos = Transform.basis.Xform(new Vector3(0, 0, dist));
-            GD.Print(pos);
             var planet = (Planet)PlanetScene.Instance();
-            var temp = planet.GlobalTransform;
+            var temp = planet.Transform;
             temp.origin = pos;
-            planet.GlobalTransform = temp;
+            planet.Transform = temp;
             AddChild(planet);
-            dist += 4;
-            angle+= 23f;
+            dist += Rand.Next(4, 10);
+            angle += Rand.Next(0,50);
         }
         Rotation = Vector3.Zero;
     }
@@ -60,9 +58,4 @@ public class StarSystem : Spatial
         Generate();
     }
 
-//  // Called every frame. 'delta' is the elapsed time since the previous frame.
-//  public override void _Process(float delta)
-//  {
-//      
-//  }
 }
