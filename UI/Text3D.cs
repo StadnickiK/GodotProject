ca1a9f23f310bdbea0a87355bf05061762b50646
StadnickiK <kstadnicki@interia.pl>
2020-09-09 19:31:46 +0200

    Increase max number of characters displayed from 4 to 13

--- UI/Text3D.cs
+++ UI/Text3D.cs
@@ -20,13 +20,22 @@ public class Text3D : Spatial
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
     {
+        LoadChildren();
+        UpdateText(Text);
+    }
+
+    void LoadChildren(){
         label = (Label)GetNode("Viewport/Label");
         vp = (Viewport)GetNode("Viewport");
         mesh = (MeshInstance)GetNode("Mesh");
+    }
+
+    public void UpdateText(String Text){
         label.Text = Text;
         vp.Size = label.RectSize;
         label.RectPosition = Vector2.Zero;
         mesh.Scale = TextScale;
+        vp.RenderTargetUpdateMode = Viewport.UpdateMode.Once;
     }
 
 //  // Called every frame. 'delta' is the elapsed time since the previous frame.
