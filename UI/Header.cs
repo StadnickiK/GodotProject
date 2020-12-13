fbb8356e55deb9db606f53efb5518905aceaa46b
StadnickiK <stakrzysztof@interia.pl>
2020-12-13 19:33:11 +0100

    Add header to UI elements

--- UI/Header.cs
+++ UI/Header.cs
@@ -5,14 +5,25 @@ public class Header : HBoxContainer
 {
     Button button = null;
 
+    Label _TitleLabel = null;
+
+    [Export]
+    public string Title { get; set; } = "Title";
+
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
     {
         GetNodes();
+        _TitleLabel.Text = Title;
+    }
+
+    public void SetTitle(string title){
+        _TitleLabel.Text = title;
     }
 
     void GetNodes(){
         button = GetNode<Button>("XButton");
+        _TitleLabel = GetNode<Label>("Title");
     }
 
 //  // Called every frame. 'delta' is the elapsed time since the previous frame.
