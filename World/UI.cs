16ff0c353aca1c058bf9d2fa8e784a8d5619c9ed
StadnickiK <stakrzysztof@interia.pl>
2020-12-13 19:31:15 +0100

    Add new items to UI node

---
+++ World/UI.cs
@@ -0,0 +1,51 @@
+using Godot;
+using System;
+
+public class UI : Node
+{
+
+
+    public ResourcePanel ResPanel { get; set; } = null;
+
+    public PlanetInterface PInterface { get; set; } = null;
+    
+    public RightPanel RPanel { get; set; } = null;
+
+    private Control _menu = null;
+    public Control WorldMenu
+    {
+        get { return _menu; }
+    }
+
+    private BattlePanel _battlePanel;
+    public BattlePanel BattlePan
+    {
+        get { return _battlePanel; }
+    }
+    
+    
+
+    void GetNodes(){
+        ResPanel = GetNode<ResourcePanel>("ResourcePanel");
+        PInterface = GetNode<PlanetInterface>("PlanetInterface");
+        RPanel = GetNode<RightPanel>("RightPanel");
+        _menu = GetNode<Control>("Menu");
+        _battlePanel = GetNode<BattlePanel>("BattlePanel");
+    }
+
+    public void UpdateUI(Player player){
+        if(player.MapObjectsChanged){
+            RPanel.UpdateRightPanel(player);
+            player.MapObjectsChanged = false;
+        }
+    }
+
+    public override void _Ready()
+    {
+        GetNodes();
+    }
+
+    //public void ConnectToLookAtObject(Node node, string methodName){
+        // to do if RPanel goes private
+    //}
+}
