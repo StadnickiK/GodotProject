1df1f23340d6345fc2007006177267d548768360
StadnickiK <stakrzysztof@interia.pl>
2020-12-13 19:37:22 +0100

    Update MainMenu and GameMenu with new ExitButton and moved to center of screen

---
+++ Menu/MainMenu.cs
@@ -0,0 +1,48 @@
+using Godot;
+using System;
+
+public class MainMenu : Control
+{
+    
+    CanvasItem _menuNode = null;
+
+    CanvasItem _newGameNode = null;
+
+    Label _TitleLabel = null;
+
+    [Signal]
+    delegate void QuickGame();
+
+    [Export]
+    string Title = "Title";
+
+    void GetNodes(){
+        _menuNode = GetNode<CanvasItem>("Menu");
+        _newGameNode = GetNode<CanvasItem>("NewGame");
+        _TitleLabel = GetNode<Label>("Menu/Label");
+        _TitleLabel.Text = Title;
+        var parent = (Game)GetParent();
+        parent.ConnectToQuickGame(this);
+    }
+
+    public override void _Ready()
+    {
+        GetNodes();
+        _newGameNode.Visible = false;
+    }
+
+    void _on_NewGame_button_up(){
+        //_menuNode.Visible = false;
+        _newGameNode.Visible = true;
+    }
+
+    void _on_Play_button_up(){
+        EmitSignal(nameof(QuickGame));
+    }
+
+//  // Called every frame. 'delta' is the elapsed time since the previous frame.
+//  public override void _Process(float delta)
+//  {
+//      
+//  }
+}
--- Menu/MainMenuButton.cs
+++ Menu/MainMenuButton.cs
@@ -1,20 +1,24 @@
 using Godot;
 using System;
 
-public class ExitButton : Button
+public class MainMenuButton : Button
 {
     // Declare member variables here. Examples:
     // private int a = 2;
     // private string b = "text";
 
+    [Signal]
+    delegate void OpenMainMenu();
+
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
     {
-        
+        var game = (Game)GetNode("/root/Game");
+        game.ConnectToOpenMainMenu(this);
     }
 
-    public void _on_Exit_button_up(){
-        GetTree().Quit();
+    public void _on_MainMenu_button_up(){
+        EmitSignal(nameof(OpenMainMenu));
     }
 
 //  // Called every frame. 'delta' is the elapsed time since the previous frame.
