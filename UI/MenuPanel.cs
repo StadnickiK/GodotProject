14ea3863bf8caed42ddb786806fc009aea105824
StadnickiK <stakrzysztof@interia.pl>
2020-12-13 19:52:49 +0100

    Move MenuPanel to center of screen

--- UI/MenuPanel.cs
+++ UI/MenuPanel.cs
@@ -1,5 +1,6 @@
 using Godot;
 using System;
+using System.Collections.Generic;
 
 public class MenuPanel : Panel
 {
@@ -7,23 +8,46 @@ public class MenuPanel : Panel
 
     Label _titleLabel = null;
 
+    Node _valueContainer = null;
+
+    Dictionary<string, int> WorldGenParameters = new Dictionary<string, int>();
+
     [Export]
     public string Title { get; set; } = "Title";
     // Called when the node enters the scene tree for the first time.
 
+    [Export(PropertyHint.File, "*.tscn")]
+    string newScene;
+
+    [Signal]
+    public delegate void StartNewGame(Dictionary<string,int> WorldGenParameters);
+
     void GetNodes(){
-        _closeButton = GetNode<Button>("VBoxContainer/Header/XButton");
-        _titleLabel = GetNode<Label>("VBoxContainer/Header/Title");
+        _closeButton = GetNode<Button>("Header/XButton");
+        _titleLabel = GetNode<Label>("Header/Title");
+        _valueContainer = GetNode<Node>("VBoxContainer/ValueContainer");
     }
 
     public override void _Ready()
     {
         GetNodes();
         _closeButton.Connect("button_up", this, nameof(_on_XButton_button_up));
+        var parent = (Game)GetParent().GetParent();
+        parent.ConnectToStartNewGame(this);
     }
 
     void _on_XButton_button_up(){
         Visible = false;
+    }
+
+    void _on_Confirm_button_up(){
+        foreach(Node n in _valueContainer.GetChildren()){
+            if(n is ValueSlider){
+                ValueSlider value = (ValueSlider)n;
+                WorldGenParameters.Add(value.ValueName, value.CurrentValue);
+            }
+        }
+        EmitSignal(nameof(StartNewGame), WorldGenParameters);
     }
 
 }
